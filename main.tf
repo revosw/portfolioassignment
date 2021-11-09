55fc0eace00e588e3241d0e8c26032460d96c44e
Simen Wiik <simenwiik@hotmail.com>
2021-11-09 19:48:24 +0100

    Change to AWS provider

--- main.tf
+++ main.tf
@@ -1,16 +1,5 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~> 2.65"
-    }
-  }
-
-  required_version = ">= 0.14.9"
-}
-
-provider "azurerm" {
-  features {}
+provider "aws" {
+  region = "eu-north-1"
 }
 
 resource "azurerm_resource_group" "rg" {
